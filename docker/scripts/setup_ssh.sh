53e3349f636a74aea1081a42b61367a6fddec1cd
Mimsy <mimsy@gitlab.com>
2020-09-02 21:53:06 +0200

    Update docker-compose file for easy deployment Fixes #10

---
+++ docker/scripts/setup_ssh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.ssh
+chmod 0700 ~/.ssh
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "" -C "$(whoami)@$(hostname)-$(date -I)"
+cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
+chmod 0640 ~/.ssh/authorized_keys
+cat <<EOT >> ~/.ssh/config
+Host *
+StrictHostKeyChecking no
+UserKnownHostsFile /dev/null
+LogLevel QUIET
+EOT
+chmod 0644 ~/.ssh/config
+
---
+++ docker/scripts/start_controller.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO change _ in - for better compliance with shell conventions
+
+# TODO: #20 ugly workaround of forcing munge to run as root ... check later
+# Run munge deamon
+echo "Starting munge deamon"
+/sbin/munged --force
+munge -n
+munge -n | unmunge
+remunge
+# Copy key into secret volume to be used again by compute nodes
+cp /etc/munge/munge.key /secrets/munge-key
+
+# Launch slurmctld
+echo "Starting slurmctld"
+slurmctld -f /etc/slurm/slurm.conf
+
+tail -f /dev/null
