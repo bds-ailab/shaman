68791467103ff9d70eed2034683afeb1cb87f106
SphRbtHyk <63612675+SphRbtHyk@users.noreply.github.com>
2020-09-22 11:09:11 +0200

    Added empty skeleton for the API

---
+++ optimization_engine/bb_wrapper/tests_integration/test_integration_api.py
@@ -0,0 +1,36 @@
+"""
+Integration testing of SHAMan within the API.
+
+This tests require:
+    - The running API
+    - The slurm workload manager
+There is of course a dependance between the API code and whether or not this script runs properly.
+
+Tests the following scenario:
+- The user creates a new component
+- The user launches an experiment
+- The user reads the experiment results
+"""
+import unittest
+
+
+class TestOptimization(unittest.TestCase):
+    """Tests the proper integration of the optimization engine within the API.
+    """
+
+    def setUp(self):
+        """Sets up the environment by sending a component to the API (running the install script)
+        """
+        # Run the installation script using the component_config file
+
+        # Check that the API properly returns the component result
+
+    def launch_experiment(self):
+        """Launch an optimization experiment.
+        """
+        # Launch an optimization experiment using SHAMan run function
+
+    def tearDown(self):
+        """Reads the results from the experiment to ensure proper running.
+        """
+        # Get the results of the experiment back by reading from the API
