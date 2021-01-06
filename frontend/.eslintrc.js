146bf2c5b555be03b8fa92759206679f341d53a4
SphRbtHyk <dev.shaman.project@gmail.com>
2021-01-06 17:05:21 +0100

    updated version of axios after security issue found

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,13 +14,15 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    "prettier/prettier": ["error", {
-     "endOfLine":"auto"
-   }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/comment-directive': 0
   }
 }
