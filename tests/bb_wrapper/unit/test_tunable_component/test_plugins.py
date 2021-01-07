db8eb482322c8a8a83955c6a5c9bc137315a6ac5
SphRbtHyk <63612675+SphRbtHyk@users.noreply.github.com>
2021-01-07 21:24:05 +0100

    Added function to perform parsing of OSU benchmark output and added corresponding test

--- tests/bb_wrapper/unit/test_tunable_component/test_plugins.py
+++ tests/bb_wrapper/unit/test_tunable_component/test_plugins.py
@@ -5,11 +5,13 @@ Module for unit-testing of the parsing plugins.
 import unittest
 from pathlib import Path
 from bb_wrapper.tunable_component.plugins.parse_execution_time import parse_milliseconds, parse_slurm_times
+from bb_wrapper.tunable_component.plugins.parse_osu_output import parse_osu_output
+
 
 TEST_SLURMS = Path(__file__).parent / "test_slurm_outputs"
 
 
-class TestPlugins(unittest.TestCase):
+class TestParseTimePlugin(unittest.TestCase):
 
     def test_parse_slurm_times(self):
         """Tests that the slurm times are properly parsed from a slurm out file.
@@ -44,6 +46,17 @@ class TestPlugins(unittest.TestCase):
         self.assertEqual(time, 61.001)
         time = parse_milliseconds("123m45.67s")
         self.assertEqual(time, 7425.067)
+
+
+class TestOSUParser(unittest.TestCase):
+    """Tests that the OSU parser behaves as expected.
+    """
+
+    def test_parse_osu_output(self):
+        """Tests that parsing the osu file behaves as expected.
+        """
+        elapsed_time = parse_osu_output("19975", path=TEST_SLURMS)
+        self.assertEqual(1003064475.26, elapsed_time)
 
 
 if __name__ == "__main__":
