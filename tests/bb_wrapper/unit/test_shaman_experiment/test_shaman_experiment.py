0471a53fcdc62e37eca59b19b45214858bf68622
SphRbtHyk <63612675+SphRbtHyk@users.noreply.github.com>
2021-02-23 15:50:58 +0100

    Changed equality to list equality

--- tests/bb_wrapper/unit/test_shaman_experiment/test_shaman_experiment.py
+++ tests/bb_wrapper/unit/test_shaman_experiment/test_shaman_experiment.py
@@ -326,7 +326,7 @@ class TestSHAManExperiment(unittest.TestCase):
         se.clean()
         # Check that calling the cleaning method moves the slurm outputs to the
         # SLURM_DIR folder
-        self.assertEqual(
+        self.assertListEqual(
             [file_.name for file_ in SLURM_DIR.glob("*")],
             ["slurm-42.out", "slurm-666.out"],
         )
